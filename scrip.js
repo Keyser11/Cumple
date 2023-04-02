494ada2e798b4f4878548aec4c23c88b31577c77
Mariobrosh44 <128196730+Mariobrosh44@users.noreply.github.com>
2023-04-02 04:01:19 -0500

    Add files via upload

---
+++ scrip.js
@@ -0,0 +1,55 @@
+function CambiaMsj() {
+    document.getElementById("loading-msj").innerText = "Enviado con Exito 🎊🎁"
+}
+
+setTimeout(CambiaMsj,12000)
+
+setTimeout(function() {
+    alert(" Tu Familia te ha deceado un Feliz Cumpleaños ❤️") 
+}, 14100)
+
+setTimeout(function() {
+    alert(" Te deseamos que la pases bien hoy en tu dia🙈😊")
+}, 14100)
+
+setTimeout(function() {
+    alert("Y que disfrutes en familia con...")
+}, 14100)
+
+setTimeout(function() {
+    alert("Tu vieja, la mas querida 🙈😚❤️")
+}, 14100)
+
+setTimeout(function() {
+    alert("Tu hija presucia 🙊😝🤭")
+}, 14100)
+
+setTimeout(function() {
+    alert("Tu bolson 💪😁")
+}, 14100)
+
+setTimeout(function() {
+    alert("Y con tu Quesooo!! (Copia) 🧀😉👊")
+}, 14100)
+
+setTimeout(function() {
+    alert("Y con el mas feo de la casa, el Karifeo (Dayko) 🐶🤞")
+}, 14100)
+
+setTimeout(function() {
+    alert("Feliz Cumpleaños Campeon!! ☺🥳🎉🎂🤞")
+}, 14100)
+
+setTimeout(function() {
+    alert(" Te Queremos Mucho 🫂☺✌❤️")
+}, 14100)
+
+setTimeout(function() {
+    alert(" ATT: Tu Familia ❤️")
+}, 14100)
+
+
+setTimeout(function() {
+    document.getElementById("BtnClick").style.display = "flex";
+}, 15000);
+
